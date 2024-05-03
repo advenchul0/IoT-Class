86b7640ef95dfa15f21f39d0dcf23d1bd3f6fcd4
advenchul0 <advenchul0@gmail.com>
2024-05-03 14:28:10 +0900

    undefined와 null의 비교

---
+++ Ch1/1-22/1-22.js
@@ -0,0 +1,9 @@
+var n = null;
+console.log(typeof n); //object (n이 object이거나 null일 수 있음)
+
+console.log(n == undefined); //true
+console.log(n == null); //true
+
+console.log(n === undefined); //false
+console.log(n === null); //true
+
---
+++ Ch1/1-22/readme.md
@@ -0,0 +1,4 @@
+## 1-22 예제 정리
+
+1. null의 type이 object라고 나오는 bug 존재 --> `typeof`로 **null 확인 불가**
+2. `null == undefined(true)` / `null === undefined(false)`
