588518684eb64b57f05b6ebcc0967e2e2c8e93ab
advenchul0 <advenchul0@gmail.com>
2024-05-03 14:17:46 +0900

    변수 복사 이후 값 변경 결과 비교(2) - 객체 자체를 변경했을 때

---
+++ Ch1/1-09/1-9.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = a; // b가 a 주소를 가르킴 --> 결국 10을 가르킴
+
+var obj1 = {c : 10, d : 'ddd'}
+var obj2 = obj1; // obj2가 obj1의 주소를 가르킴 --> reference data type --> 10, ddd 가르킴
+
+b = 15; // 15 새로운 data 할당, b의 identifer adress가 15의 주소를 가르킴
+obj2 = {c : 20, d : 'ddd'}
+
+
+ 
