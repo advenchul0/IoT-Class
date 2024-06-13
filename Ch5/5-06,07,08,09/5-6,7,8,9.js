602822aea02b189d72f9040c4d52f042a610e93f
advenchul0 <advenchul0@gmail.com>
2024-06-13 10:37:18 +0900

    callback function & closure (1, 2, 3, 4)

---
+++ Ch5/5-06,07,08,09/5-6,7,8,9.js
@@ -0,0 +1,64 @@
+//5-6
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul'); // (공통 코드)
+
+fruits.forEach(function(fruit) {
+  // (A)
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', function() {
+    // (B)
+    alert('your choice is ' + fruit);
+  });
+  $ul.appendChild($li);
+});
+document.body.appendChild($ul); 
+
+//5-7
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+var alertFruit = function(fruit) {
+  alert('your choice is ' + fruit);
+};
+fruits.forEach(function(fruit) {
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', alertFruit);
+  $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+alertFruit(fruits[1]);
+
+//5-8
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+var alertFruit = function(fruit) {
+  alert('your choice is ' + fruit);
+};
+fruits.forEach(function(fruit) {
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', alertFruit.bind(null, fruit));
+  $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+
+
+//5-9
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+var alertFruitBuilder = function(fruit) {
+  return function() {
+    alert('your choice is ' + fruit);
+  };
+};
+fruits.forEach(function(fruit) {
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', alertFruitBuilder(fruit));
+  $ul.appendChild($li);
+});
+document.body.appendChild($ul);
---
+++ Ch5/5-06,07,08,09/readme.md
@@ -0,0 +1,12 @@
+## 5-6,7,8,9 예제 정리
+
+1. `$ul`은 html의 element (.으로 열거)
+2. `$li`는 html의 element (숫자로 열거)
+3. `document`는 html을 의미
+4. `5-7` 예제의 경우에서는 `addEventListener`은 `alertFruit`에 **event**를 넣어주므로, 원하는 대로 과일을 출력하지 않음
+5. `5-8` 처럼 bind method를 사용하여, fruit를 **bound argument** 형태로 입력해주믄 원하는 방식으로 사용 가능
+6. `5-8`에서의 `alertFruit` &rarr; `bound alertFruit`
+7. `5-9`와 같이 특정 함수를 여러번 사용하고 싶을때, fruit과 같이 변하는 변수를 상수 처럼 사용(closure 변수로 값 제공) 가능 
+
+
+
